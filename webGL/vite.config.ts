7085cd7c95c95fb8f4c2c1454f3e5a66bcba8cfa
Lorenzo <wangjp@jiguang.cn>
2021-11-11 14:28:02 +0800

    fix

---
+++ webGL/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
+// import path from "path";
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: process.cwd(),
+  base: "./",
+
+  plugins: [react(), legacy()],
+  alias: {
+    "@": "/src",
+    "@components": "/src/components",
+    "@utils": "src/utils"
+  },
+  build: {
+    outDir: "dist",
+    assetsInlineLimit: 4096, // 4KB base64地址大小
+    cssCodeSplit: true, // css 文件拆分 否则会打包到一个css文件中
+    sourcemap: false //是否生成sourcemap
+  }
+});
